32dd9054180d034258c5b05996e6c6ebe98c57af
Yooj <1940576083@qq.com>
2021-12-09 01:54:44 +0800

    Commit for implementation of loader loading the OS kernel and the simple implementation of kernel.

---
+++ HandwritingOS/source/kernel/main.c
@@ -0,0 +1,13 @@
+/*
+* @Author: Yooj
+* @Date:   2021-12-08 01:25:50
+* @Last Modified by:   Yooj
+* @Last Modified time: 2021-12-08 01:36:50
+*/
+
+// int _start(void) // Linux链接默认程序入口函数名
+int main(void)
+{
+    while (1);
+    return 0;
+}
